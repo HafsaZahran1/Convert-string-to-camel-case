0cf433b3b1e0d17e3be2b8564dc75d15b161c59b
HafsaZahran1 <73903183+HafsaZahran1@users.noreply.github.com>
2021-07-01 20:52:05 +0200

    Add files via upload

---
+++ Camel_Case/camel _case/camel _case/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+public class Kata
+{
+   static void Main()
+    {
+        Console.WriteLine(ToCamelCase(Console.ReadLine()));
+    }
+
+    public static string ToCamelCase(string str)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] != '-' && str[i] != '_') sb.Append(str[i]);
+            else sb.Append(char.ToUpper(str[++i]));
+        }
+        return sb.ToString();
+    }
+}
+
